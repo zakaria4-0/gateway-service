9e3a70da21f9f02ae135b50e64dc279179a46787
Zkerouad <zakaria.kerouad1999@gmail.com>
2024-01-23 16:04:24 +0100

    first MicroService app commit

---
+++ src/main/java/net/kerouad/gatewayservice/GatewayServiceApplication.java
@@ -0,0 +1,24 @@
+package net.kerouad.gatewayservice;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.context.annotation.Bean;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceApplication.class, args);
+    }
+
+    @Bean
+    DiscoveryClientRouteDefinitionLocator locator(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties dlp){ // il permet de faire le routage dynamique
+        return new DiscoveryClientRouteDefinitionLocator(rdc, dlp);
+    }
+}
